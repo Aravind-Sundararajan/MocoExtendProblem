360edd839c0ca77a745226b91b583a8a297118cc
Varun Joshi <joshi.142@osu.edu>
2022-03-28 13:40:44 -0400

    Added end-point constraint mode to activation squared goal
    The end-point constraint mode allows a user to force the sum of squared activations to match a target value. The target value is 0 by default, but can be set using the setEndPointGoal() function.

--- MocoActivationSquaredGoal/MocoActivationSquaredGoal.h
+++ MocoActivationSquaredGoal/MocoActivationSquaredGoal.h
@@ -7,7 +7,6 @@
  * Author(s): Varun Joshi                                                     *
  * -------------------------------------------------------------------------- */
 
-
 #include <OpenSim/Moco/osimMoco.h>
 #include "osimMocoActivationSquaredGoalDLL.h"
 
@@ -17,6 +16,7 @@ class OSIMMOCOACTIVATIONSQUAREDGOAL_API MocoActivationSquaredGoal : public MocoG
     OpenSim_DECLARE_CONCRETE_OBJECT(MocoActivationSquaredGoal, MocoGoal);
 
 public:
+    // Make the constructors for this class
     MocoActivationSquaredGoal() { constructProperties();}
     MocoActivationSquaredGoal(std::string name) : MocoGoal(std::move(name)) {
         constructProperties();
@@ -26,27 +26,41 @@ public:
         constructProperties();
     }
 
-    // Public members to change the divide by displacement property
+    // Public methods to change and get the divide by displacement property
     void setDivideByDisplacement(bool tf) { set_divide_by_displacement(tf); }
-    bool getDivideByDisplacement() const {
-        return get_divide_by_displacement();
-    }
+    bool getDivideByDisplacement() const { return get_divide_by_displacement(); }
+
+    // Public methods to change and get the end-point goal value
+    void setEndPointGoal(double end_point_goal) { set_end_point_goal(end_point_goal); }
+    double getEndPointGoal() const { return get_end_point_goal(); }
+
 
 protected:
     Mode getDefaultModeImpl() const override { return Mode::Cost; }
-    bool getSupportsEndpointConstraintImpl() const override { return false;}
+    bool getSupportsEndpointConstraintImpl() const override { return true;}
+
+    // Initialization function
     void initializeOnModelImpl(const Model&) const override;
+    
+    // Integration functions
     void calcIntegrandImpl(
             const IntegrandInput& input, double& integrand) const override;
     void calcGoalImpl(
             const GoalInput& input, SimTK::Vector& cost) const override;
 
  private:
+    // Make the divide by displacement property
     OpenSim_DECLARE_PROPERTY(divide_by_displacement, bool,
         "Divide by the model's displacement over the phase (default: "
         "false)");
+    // Make the end_point_goal property
+    OpenSim_DECLARE_PROPERTY(end_point_goal, double,
+        "Target value for end-point goal (default: 0)");
+
+    // Make a function to set the default values of these properties
     void constructProperties();
 
+    // Make a private member that stores all the indices for muscle activations
     mutable std::vector<int> m_act_indices;
 };
 
