007217e6014fc7554e9ad374071df14fa15763c6
Varun Joshi <joshi.142@osu.edu>
2022-03-29 11:19:56 -0400

    New goal to minimize the acceleration of a target marker
    Minimizes the sum of the magnitudes of acceleration in all three coordinates (X,Y,Z). Marker names are added in the form "/markerset/markername".

---
+++ MocoMarkerAccelerationGoal/osimMocoMarkerAccelerationGoalDLL.h
@@ -0,0 +1,20 @@
+#ifndef OPENSIM_OSIMMOCOMARKERACCELERATIONGOALDLL_H
+#define OPENSIM_OSIMMOCOMARKERACCELERATIONGOALDLL_H
+/* -------------------------------------------------------------------------- *
+ * OpenSim: osimMocoMarkerAccelerationGoalDLL.h                               *
+ * -------------------------------------------------------------------------- *
+ *                                                                            *
+ * Author(s): Varun Joshi                                                     *
+ * -------------------------------------------------------------------------- */
+
+#ifndef _WIN32
+    #define OSIMMOCOMARKERACCELERATIONGOAL_API
+#else
+    #ifdef OSIMMOCOMARKERACCELERATIONGOAL_EXPORTS
+        #define OSIMMOCOMARKERACCELERATIONGOAL_API __declspec(dllexport)
+    #else
+        #define OSIMMOCOMARKERACCELERATIONGOAL_API __declspec(dllimport)
+    #endif
+#endif
+
+#endif // OPENSIM_OSIMMOCOMARKERACCELERATIONGOALDLL_H
