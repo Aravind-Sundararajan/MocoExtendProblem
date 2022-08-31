16e3f8f230951b8833c3e794b1d3f648d51f4e09
Aravind-Sundararajan <Aravind.Sundararajan@protonmail.com>
2022-08-30 17:01:13 -0700

    solved issue, the string for goalName can't have spaces for some reason.

--- utils/generate_content.m
+++ utils/generate_content.m
@@ -7,16 +7,16 @@ wrap_out = "";
         libPath = pwd+"/custom_goals/"+goal+"/"+goal+".h";
         setters = get_setter_functions(libPath);
         %each goal has a name and weight, though things like path constraints don't have a weight.
-        goalfun = "    void add"+ goal +"(const string& goalName, double weight";
+        goalfun = "    void add"+ goal +"(const std::string& goalName, double weight";
         mex_add_def = ...
             "    MEX_DEFINE(add"+goal+") (int nlhs, mxArray* plhs[], int nrhs, const mxArray* prhs[])"+newline+...
             "    {"+newline+...
             "        InputArguments input(nrhs, prhs, "+num2str(length(fields(setters))+3)+");"+newline+...
             "        OutputArguments output(nlhs, plhs, 0);"+newline+...
             "        extendProblem* engine = Session<extendProblem>::get(input.get(0));"+newline+...
-            "        engine->add"+goal+"(input.get<string>(1),input.get<double>(2)";
-        f_wrap = "        function add"+goal+"(this,goalname,weight";
-        wrapfun = "            extendProblem('add"+goal+"', this.id_,goalname,weight";
+            "        engine->add"+goal+"(input.get<std::string>(1),input.get<double>(2)";
+        f_wrap = "        function add"+goal+"(this,goalName,weight";
+        wrapfun = "            extendProblem('add"+goal+"', this.id_,goalName,weight";
         set_calls = struct();
         i = 1;
         for s = string(fields(setters))'
@@ -29,7 +29,7 @@ wrap_out = "";
         end
         
         %MATLAB class def
-        f_wrap = f_wrap + ")";
+        f_wrap = f_wrap + ")" + newline + "           assert(isscalar(weight))";
         wrapfun = wrapfun + ");";
         wrap_out = wrap_out + f_wrap + newline;
         wrap_out = wrap_out + wrapfun + newline;
@@ -44,7 +44,7 @@ wrap_out = "";
         cpp_out = cpp_out +goalfun + newline;
         cpp_out = cpp_out +"        mexPrintf(""Adding "+goal+" goal\n"");" + newline;
         cpp_out = cpp_out +"        auto* goal = m_p->addGoal<"+goal+">(goalName, weight);" + newline;
-        
+        cpp_out = cpp_out +"        goal->setName(goalName);" + newline;
         for s = string(fields(set_calls))'
             cpp_out = cpp_out +set_calls.(s) + newline;
         end
